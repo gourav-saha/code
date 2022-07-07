9b21c77b5875394a5f6c28f5ae282c2e295788e3
Gourav Saha <gouravjsaha@gmail.com>
2022-07-07 19:44:17 -0400

    kinda wrong

---
+++ orthologues.R
@@ -0,0 +1,7 @@
+humaninyeast <- read.csv('humaninyeast.csv')
+yeastinhuman <- read.csv('yeastinhuman.csv')
+interactome <- read.csv('interactome_bi.csv')
+
+head(yeastinhuman)
+
+humaninyeast[humaninyeast$Human.GeneID ==100506658]
